3250db8b48a1879548c61e3f01d4409316ae50d5
dhiren-joshi <130563022+dhiren-joshi@users.noreply.github.com>
2025-06-08 19:16:09 +0000

    code and notes for datatype

---
+++ basics/datatypes.js
@@ -0,0 +1,38 @@
+"use strict";//treat all js code as newer version but nowdays its like on default but can be a good practice to write this
+
+//we are using nodejs not browser
+// alert(3+3)
+
+let name ="dhiren"
+let number= 99
+let issleeping  = false
+let state = null 
+
+/*datatypes notes
+Seven data types that are primitives:
+
+Boolean=> true and false.
+
+null=> A special keyword denoting a null value.-->standalone value (Because JavaScript is case-sensitive, null is not the same as Null, NULL, or any other variant.)
+
+undefined=> A top-level property whose value is not defined.
+
+Number=> An integer or floating point number. For example: 42 or 3.14159.
+
+BigInt=> An integer with arbitrary precision. For example: 9007199254740992n.
+
+String[""]=> A sequence of characters that represent a text value. For example: "Howdy".
+
+Symbol- A data type whose instances are unique and immutable.
+
+undefined=> abhi value assign nahi hui hai
+
+symbol=> used for unique
+
+*/
+
+//object
+
+console.log(typeof "dhiren") //output-->string
+console.log(typeof undefined) //output-->undefined
+console.log(typeof null)//o/p--> object
