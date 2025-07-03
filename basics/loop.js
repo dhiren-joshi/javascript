6b1ef01d900aca5c31a8b6009d7401fd3e6a0678
dhiren-joshi <130563022+dhiren-joshi@users.noreply.github.com>
2025-07-03 13:50:04 +0000

    updated loops

--- basics/loop.js
+++ basics/loop.js
@@ -60,11 +60,12 @@
 if(useremail.length===0){
    console.log("array is empty");
 }
+/*
 check object is empty={}
 if(Object.keys(emptyObj).length===0){
    console.log("object is empty");
 }
-/*nulish coalescing operator(??):null undefined
+nulish coalescing operator(??):null undefined
 let val1;
 val1= 5 ?? 10
 val1 =null ?? 10
